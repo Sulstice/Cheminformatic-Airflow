523b219c30c2b936482a05de2fd4fa395d71d798
Sul <sharifsuliman1@gmail.com>
2022-03-21 13:04:38 -0400

    Update plotting_app_config.py

--- plugins/plotting_config/plotting_app_config.py
+++ plugins/plotting_config/plotting_app_config.py
@@ -20,33 +20,6 @@ class PCAAnalysisAppBuilderBaseView(AppBuilderBaseView):
     
         return self.render_template("pca_analysis.html")
 
-class ProbabilityDistributionAppBuilderBaseView(AppBuilderBaseView):
-
-    template_folder = '/home/sulstice/airflow/plugins/plotting_plugins/templates'
-
-    @expose("/")
-    def list(self):
-
-        return self.render_template("probability_distribution.html")
-
-class CGenFFCompassAppBuilderBaseView(AppBuilderBaseView):
-
-    template_folder = '/home/sulstice/airflow/plugins/plotting_plugins/templates'
-    
-    @expose("/")
-    def list(self):
-
-        return self.render_template("cgenff_compass.html")
-
-class ZMatrixStoreAppBuilderBaseView(AppBuilderBaseView):
-
-    template_folder = '/home/sulstice/airflow/plugins/plotting_plugins/templates'
-
-    @expose("/")
-    def list(self):
-        
-        return self.render_template("zmatrix_store.html")
-
 pca_analysis_appbuilder_view = PCAAnalysisAppBuilderBaseView()
 pca_analysis_appbuilder_package = {
         "name": "PCA Analysis",
@@ -54,26 +27,6 @@ pca_analysis_appbuilder_package = {
         "view": pca_analysis_appbuilder_view
 }
 
-probability_distribution_appbuilder_view = ProbabilityDistributionAppBuilderBaseView()
-probability_distribution_appbuilder_package = {
-        "name": "Probability Distribution Penalty Scores",
-        "category": "Pipeline Plots",
-        "view": probability_distribution_appbuilder_view
-}
-
-cgenff_compass_appbuilder_view = CGenFFCompassAppBuilderBaseView()
-cgenff_compass_appbuilder_package = {
-        "name": "CGenFF Compass",
-        "category": "Pipeline Plots",
-        "view": cgenff_compass_appbuilder_view
-}
-
-z_matrix_appbuilder_view = ZMatrixStoreAppBuilderBaseView()
-z_matrix_appbuilder_package = {
-        "name": "Z-Matrix Store",
-        "category": "Pipeline Plots",
-        "view": z_matrix_appbuilder_view,
-}
 
 
 # Defining the plugin class
@@ -82,6 +35,6 @@ class AirflowTestPlugin(AirflowPlugin):
     # operators = []
     # flask_blueprints = [bp]
     # hooks = []
-    appbuilder_views = [pca_analysis_appbuilder_package, probability_distribution_appbuilder_package, cgenff_compass_appbuilder_package, z_matrix_appbuilder_package]
+    appbuilder_views = [pca_analysis_appbuilder_package]
     # executors = []
     # admin_views = []
